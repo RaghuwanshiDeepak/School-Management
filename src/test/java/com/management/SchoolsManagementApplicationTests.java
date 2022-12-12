91dfe0e2ac8293e337a590c13dbcde4f9cbe3d79
RaghuwanshiDeepak <raghuwanshideepak355@gmail.com>
2022-12-12 16:34:47 +0530

    added some modification

--- src/test/java/com/management/SchoolsManagementApplicationTests.java
+++ src/test/java/com/management/SchoolsManagementApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SchoolManagementApplicationTests {
+class SchoolsManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
